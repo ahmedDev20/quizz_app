3716cde5f3ec9181f3f4ed3544317c34ee5d5161
Ahmed Balady <baladyahmed19@gmail.com>
2022-12-14 12:34:16 +0100

    fixing unit test

--- test/quiz_state_test.dart
+++ test/quiz_state_test.dart
@@ -1,11 +1,11 @@
 import 'package:quizz_app/screens/quiz/quiz_state.dart';
-import 'package:flutter_test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('State progress should be 10', () {
     final state = QuizState();
 
-    state.progress = 1;
+    state.progress = 10;
 
     expect(state.progress, 10);
   });
