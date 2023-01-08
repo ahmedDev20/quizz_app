1b2df2015d0d4ea886ffdc69175f265c3cee2019
elfaraji <mohmedfaraji5@gmail.com>
2023-01-08 15:24:36 +0100

    first commit

--- test/quiz_state_test.dart
+++ test/quiz_state_test.dart
@@ -1,5 +1,9 @@
 import 'package:quizz_app/screens/quiz/quiz_state.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quizz_app/services/db.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quizz_app/screens/admin/questions/add_question.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   test('State progress should be 10', () {
@@ -16,5 +20,16 @@ void main() {
     state.progress = -5;
 
     expect(state.progress, -5);
+  });
+
+  test('getAllUser should return a list of users', () async {
+    DBService db = DBService();
+    final users = await db.getAllUsers();
+    expect(users, isA<List<User>>());
+  });
+
+  testWidgets('AddQuestion should have a title', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: AddQuestionScreen()));
+    expect(find.text('Add Question'), findsOneWidget);
   });
 }
